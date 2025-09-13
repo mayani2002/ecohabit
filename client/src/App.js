7206bd3b2fef10ddb4638205568e269a99c2c6a0
AnushaK <anushakadlimatti34@gmail.com>
2025-09-13 09:02:24 +0530

    Removed unwanted lines

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import { LoginContextProvider } from "./contexts/LoginContext.js";
 import { RegisterContextProvider } from "./contexts/RegisterContext";
 import { UserContextProvider } from "./contexts/UserContext";
 
-/**
- *To avoid bloated App.js component, page routes created at Routes.js component.
- */
 
 function App() {
   return (
